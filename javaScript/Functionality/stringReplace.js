244614c706dc560720be6ebdcee1a2a4f1353c75
kanush085 <anushk136@gmail.com>
2019-01-23 18:47:30 +0530

    first commt

--- javaScript/Functionality/stringReplace.js
+++ javaScript/Functionality/stringReplace.js
@@ -1,3 +1,8 @@
+/*
+ *@author name:Anush Kumar s k
+ *@version 1.0
+ *@purpose stringReplace
+ */
 var A=require('../Utility/utility');
 var read=require('readline-sync');
 var name=read.question("Enter your name: ");
--- javaScript/Utility/utility.js
+++ javaScript/Utility/utility.js
@@ -2,15 +2,32 @@
 const readline = require('readline-sync');
 
 
-module.exports= {
+module.exports = {
+    /***** String replace *****/
+    /*
+     * 1. username wit minmum 3 characters and not a number 
+     * with userinput
+     * 
+     
+     * @descriptipn: Declaring the function and passing the userinput as argument.
+     * 
+     * 
+     * @purpose: display the username with given inputs
+     *
+     */
+    stringReplace(username) {
+        if (username.length > 3) {
 
-    stringReplace(username) 
-{
-    var input ="Hello <<username>> , how are you?";
-    var output = input.replace(/<<username>>/g,username); 
-    console.log(output+" : Replace string using Regex ")
 
-    
-},
+            var input = "Hello <<username>> , how are you?";
+            var output = input.replace(/<<username>>/g, username);
+            console.log(output + " : Replace string using Regex ")
+        }
+        else {
+            console.log("enter minmum 3 char or more");
+
+        }
+
+    },
 
 }
